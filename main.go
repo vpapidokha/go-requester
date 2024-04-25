6b55fa0c64869d137fd512b6255565dde2fa2f45
Vladyslav Papidokha <vladyslav.papidokha@gmail.com>
2024-04-26 01:32:29 +0300

    Added logger

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"math/rand"
 	"net/http"
 	"os"
@@ -9,20 +9,30 @@ import (
 )
 
 func requester(url string) {
-	defer fmt.Println("Request sent...")
+	defer log.Println("Request sent...")
 	response, error := http.Get(url)
 	if error != nil {
-		fmt.Println(error)
+		log.Println(error)
 	}
-	fmt.Println(response)
+	log.Println(response)
 
 	n := rand.Intn(10)
-	fmt.Printf("Sleeping %d seconds...\n", n)
+	log.Printf("Sleeping %d seconds...\n", n)
 	time.Sleep(time.Duration(n) * time.Second)
 }
 
 func main() {
 	targetUrl := os.Args[1]
+
+	LOG_FILE := "/var/log/go-requester.log"
+	logFile, err := os.OpenFile(LOG_FILE, os.O_APPEND|os.O_RDWR|os.O_CREATE, 0644)
+	if err != nil {
+		log.Panic(err)
+	}
+	defer logFile.Close()
+
+	log.SetOutput(logFile)
+	log.SetFlags(log.Lshortfile | log.LstdFlags)
 
 	for {
 		requester(targetUrl)
